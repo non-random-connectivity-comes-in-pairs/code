bc2f7dd4911e871fa357bd6e5c4baf170f4688b4
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-11-17 22:24:03 +0100

    setup lab

---
+++ access_lab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -it -p 127.0.0.1:8016:8000 \
+       --user="$(id -u):$(id -g)" \
+       -v $(pwd):/home/lab \
+       felix11h/scipy3_env \
+       /bin/bash -c \
+       'screen -d -m /home/docker/env/neurosci/bin/smtweb --allips;
+        source startup_messg.sh;
+        bash'
