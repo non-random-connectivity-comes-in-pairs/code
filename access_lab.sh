0cf295af4eedc3be2b35e0b770ddef8fe4b2550e
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-11-18 14:31:55 +0100

    run smtweb on startup

--- access_lab.sh
+++ access_lab.sh
@@ -5,6 +5,6 @@ docker run -it -p 127.0.0.1:8016:8000 \
        -v $(pwd):/home/lab \
        felix11h/scipy3_env \
        /bin/bash -c \
-       'screen -d -m /home/docker/env/neurosci/bin/smtweb --allips;
+       'screen -d -m smtweb --allips;
         source startup_messg.sh;
         bash'
