df4f2f04f0aea6677ce4b46abfbc2d7963c67438
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2016-12-06 11:36:08 +0100

    post clean-up

--- fig3A.py
+++ fig3A.py
@@ -5,7 +5,9 @@ import pylab as pl
 
 import numpy as np
 from gamma_functions import fT, K
+
 from scipy import integrate
+from scipy.stats import norm
 
 from matplotlib import rc
 
@@ -19,119 +21,62 @@ pl.rcParams['text.latex.preamble'] = [
 ]  
 
 
+def gamma_sig_graph(alph, bet, xloc, sig):
+    '''
+    return graph of f^T_{\alpha,\beta}(y)
+    ----
+    alph : shape parameter alpha
+    bet  : scale parameter beta
+    xloc : already determined probability x
+    sig  : width of modulating normal distribution
+    '''
+
+    ys      = np.arange(0,1.+0.001, 0.001)
+    fT_vals = []
+    
+    # compute normalization factor only once
+    Nf = integrate.quad(lambda s: fT(s,alph,bet)
+                         *norm.pdf(s,loc=xloc, scale=sig),0,1)[0]
+
+    for y in ys:
+        fT_vals.append(fT(y, alph, bet)
+                       *norm.pdf(y,loc=xloc,scale=sig)/Nf)
+
+    return ys, fT_vals
+
+
 fig, ax = pl.subplots(1,1)
 fig.set_size_inches(7.5*0.5,2.3)
 
-#fig.suptitle(r'$\mu = 0.1$', fontsize=15)
-
-xs = np.arange(0,1.+0.001, 0.001)
-
-linestyles = ['-',':','--','-.']
-
-from scipy.stats import gamma
-from scipy.stats import norm
-
-
+# from Figure 2
 a_ns =  [0.248, 1.0, 2.0, 15.0]
 b_ns =  [0.486852833106356, 0.10004560945459162,
          0.0500000206118813, 0.006666666666666667] 
 
-# idx = 0
-# xloc = 0.15
-# sig = 0.055
-
-# idx =0
-# xloc = 0.085
-# sig = 0.035
-
-
-# idx =0
-# xloc = 0.15
-# sig = 0.0005
-
-# ts = []
-# Nf = integrate.quad(lambda s: fT(s,a_ns[idx],b_ns[idx])*norm.pdf(s,loc=xloc, scale=sig),0,1)[0]
-# for x in xs:
-#     ts.append(fT(x, a_ns[idx], b_ns[idx]) *norm.pdf(x,loc=xloc,scale=sig)/Nf)
-
-
-idx =0
+alph = a_ns[0]
+bet  = b_ns[0]
 xloc = 0.15
-sig = 0.025
-
-rs = []
-Nf = integrate.quad(lambda s: fT(s,a_ns[idx],b_ns[idx])*norm.pdf(s,loc=xloc, scale=sig),0,1)[0]
-for x in xs:
-    rs.append(fT(x, a_ns[idx], b_ns[idx]) *norm.pdf(x,loc=xloc,scale=sig)/Nf)
 
 
-idx =0
-xloc = 0.15
-sig = 1.
-
-us = []
-Nf = integrate.quad(lambda s: fT(s,a_ns[idx],b_ns[idx])*norm.pdf(s,loc=xloc, scale=sig),0,1)[0]
-for x in xs:
-    us.append(fT(x, a_ns[idx], b_ns[idx]) *norm.pdf(x,loc=xloc,scale=sig)/Nf)
-
-# ys = []
-# for x in xs:
-#     ys.append(fT(x, a_ns[idx], b_ns[idx]))
-
-idx =0
-xloc = 0.15
-sig = 0.065
-    
-zs = []
-Nf = integrate.quad(lambda s: fT(s,a_ns[idx],b_ns[idx])*norm.pdf(s,loc=xloc, scale=sig),0,1)[0]
-for x in xs:
-    zs.append(fT(x, a_ns[idx], b_ns[idx]) *norm.pdf(x,loc=xloc,scale=sig)/Nf)
-
-zcs = []
-for x in xs:
-    zcs.append(fT(x, a_ns[idx], b_ns[idx]) *norm.pdf(x,loc=xloc,scale=sig))
-    
-ws = []
-for x in xs:
-    ws.append(norm.pdf(x,loc=xloc,scale=sig))
-
-#ax.plot(xs,ts, 'k', linestyle="-.", label=r'$\sigma = 0.005$')
-ax.axvline(x=0.15, ymin=0., ymax=16., color='k', linestyle=":", label=r'$\sigma \to 0$')
-ax.plot(xs,rs, 'k', linestyle='-.', label=r'$\sigma = 0.025$')
-ax.plot(xs,zs, 'k', linestyle='--', label=r'$\sigma=0.065$')
-ax.plot(xs,us, 'k', linestyle='-', label=r'$\sigma=1$')
-#ax.plot(xs,ys)
-#ax.plot(xs,zcs)
-#ax.plot(xs,ws)
+ax.axvline(x=0.15, ymin=0., ymax=16., color='k', linestyle=":",
+           label=r'$\sigma \to 0$')
+ax.plot(*gamma_sig_graph(alph,bet,xloc, 0.025),
+        'k', linestyle='-.', label=r'$\sigma = 0.025$')
+ax.plot(*gamma_sig_graph(alph,bet,xloc, 0.065),
+        'k', linestyle='--', label=r'$\sigma = 0.065$')
+ax.plot(*gamma_sig_graph(alph,bet,xloc, 1.),
+        'k', linestyle='-', label=r'$\sigma = 1$')
 
 ax.set_ylim(0,16)
 ax.set_xlim(0,0.4)
 
 pl.xticks([0.,0.15,0.3,0.4], ['0.0', r'$x=0.15$', '0.3','0.4'])
-ax.set_title(r'$f_{P_{ij}}(y) = f_{\alpha,\beta}^T(y),\, \alpha=0.248$', size=13.)
+ax.set_title(r'$f_{P_{ij}}(y) = f_{\alpha,\beta}^T(y),\, \alpha=0.248$',
+             size=13.)
 
 ax.set_ylabel(r'$f_{P_{ji} | P_{ij}}(y \mid x)$')
 ax.set_xlabel(r'connection probability $y$')
-# ax_left.set_xlim(0,0.3)
-# ax_left.set_ylim(0,16)
-# ax_left.legend()
 
-# label = ax_left.set_xlabel(r'connection probability $P_{ij}$')
-# ax_left.xaxis.set_label_coords(0.8, -0.135)
-# ax_left.set_ylabel(r'$f(P_{ij})$')
-
-
-# for a,b,rho,ls in zip(a_ns, b_ns,rhos,linestyles):
-
-#     ax_right.plot(x, gamma.pdf(x, a, scale=b), 'k', linestyle=ls)
-
-# ax_right.set_xlim(0.3,1.0)
-# ax_right.set_ylim(0,0.5)
-
-# ax_right.set_xticks([0.3, 0.5, 0.8, 1.0])
-# ax_right.set_yticks([0., 0.1, 0.2, 0.3,0.4, 0.5])
-
-# ax_right.yaxis.tick_right()
 pl.legend(prop={'size':12})
 
 pl.savefig('fig3A.pdf', dpi=600, bbox_inches='tight')
